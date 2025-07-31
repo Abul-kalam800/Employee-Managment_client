29b4b7dfad8bfd180510a542a4231290ef8607b3
abul kalam <bd.abulkalam800@gamil.com>
2025-07-31 13:10:19 +0300

    some issue fixed

--- src/Dashboard/payment/PaymentHistory.jsx
+++ src/Dashboard/payment/PaymentHistory.jsx
@@ -9,10 +9,9 @@ const PaymentHistory = () => {
   const { user } = useAuth();
   const email = user?.email;
 
-  
   const axioesInstance = useAxios();
   const [page, setPage] = useState(1);
-  const { data: payments=[], isLoading } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ["paymentHistoty", email, page],
     queryFn: async () => {
       const res = await axioesInstance.get(
@@ -21,13 +20,12 @@ const PaymentHistory = () => {
       return res.data;
     },
   });
- 
 
   if (isLoading) return <LoadingSpnieer></LoadingSpnieer>;
 
   return (
     <>
-    <title>Payment-history</title>
+      <title>Payment-history</title>
       <h2 className="text-2xl md:text-4xl font-bold mb-10 text-center pb-5 border-dotted border-b-4 border-blue-500 md:w-6/12 mx-auto p-2">
         Payment History
       </h2>
@@ -37,65 +35,67 @@ const PaymentHistory = () => {
         hr
       </p>
 
-        {payments.length <1 ? <>
-         <p className="my-10 px-5 w-full text-center text-lg md:text-2xl font-semibold text-blue-400">
-          Sorry! You are not getting any salary after get salary then you see your payment history 
-
-         </p>
-          </>:<>
-        <div className="mt-6 px-10 ">
-          <div className="overflow-x-auto">
-            <table className="w-full border text-left">
-              <thead>
-                <tr className="bg-purple-600 text-white">
-                  <th className="border px-2 py-1">Month</th>
-                  <th className="border px-2 py-1">Year</th>
-                  <th className="border px-2 py-1">Amount</th>
-                  <th className="border px-2 py-1">Transaction ID</th>
-                </tr>
-              </thead>
-              <tbody>
-                {payments.map((payment) => (
-                  <tr key={payment._id}>
-                    <td className="border px-2 py-1">{payment.month}</td>
-                    <td className="border px-2 py-1">{payment.year}</td>
-                    <td className="border px-2 py-1">${payment.salary}</td>
-                    <td className="border px-2 py-1">
-                      {payment.transactionId}
-                    </td>
+      {data.payments.length < 1 ? (
+        <>
+          <p className="my-10 px-5 w-full text-center text-lg md:text-2xl font-semibold text-blue-400">
+            Sorry! You are not getting any salary after get salary then you see
+            your payment history
+          </p>
+        </>
+      ) : (
+        <>
+          <div className="mt-6 px-10 ">
+            <div className="overflow-x-auto">
+              <table className="w-full border text-left">
+                <thead>
+                  <tr className="bg-purple-600 text-white">
+                    <th className="border px-2 py-1">Month</th>
+                    <th className="border px-2 py-1">Year</th>
+                    <th className="border px-2 py-1">Amount</th>
+                    <th className="border px-2 py-1">Transaction ID</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-          {/* Pagination Controls */}
-          <div className="flex justify-between items-center mt-4">
-            <button
-              onClick={() => setPage((old) => Math.max(old - 1, 1))}
-              disabled={page === 1}
-              className="px-3 py-1 bg-gray-300 rounded hover:bg-gray-400"
-            >
-              Previous
-            </button>
+                </thead>
+                <tbody>
+                  {data.payments.map((payment) => (
+                    <tr key={payment._id}>
+                      <td className="border px-2 py-1">{payment.month}</td>
+                      <td className="border px-2 py-1">{payment.year}</td>
+                      <td className="border px-2 py-1">${payment.salary}</td>
+                      <td className="border px-2 py-1">
+                        {payment.transactionId}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+            {/* Pagination Controls */}
+            <div className="flex justify-between items-center mt-4">
+              <button
+                onClick={() => setPage((old) => Math.max(old - 1, 1))}
+                disabled={page === 1}
+                className="px-3 py-1 bg-gray-300 rounded hover:bg-gray-400"
+              >
+                Previous
+              </button>
 
-            <p>
-              Page {page} of {data.totalPages}
-            </p>
+              <p>
+                Page {page} of {data.totalPages}
+              </p>
 
-            <button
-              onClick={() =>
-                setPage((old) => (old < data.totalPages ? old + 1 : old))
-              }
-              disabled={page >= data.totalPages}
-              className="px-3 py-1 bg-gray-300 rounded hover:bg-gray-400"
-            >
-              Next
-            </button>
+              <button
+                onClick={() =>
+                  setPage((old) => (old < data.totalPages ? old + 1 : old))
+                }
+                disabled={page >= data.totalPages}
+                className="px-3 py-1 bg-gray-300 rounded hover:bg-gray-400"
+              >
+                Next
+              </button>
+            </div>
           </div>
-        </div>
-        
-        </>}
-     
+        </>
+      )}
     </>
   );
 };
